6198e771b71ad1fa240bf54d47315837ed18bb3a
manoj kumar <mk7840823499@gmail.com>
2024-02-23 23:07:07 +0530

    first commit

---
+++ src/App1.jsx
@@ -0,0 +1,13 @@
+import { useEffect, useState } from 'react'
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <h1 className='text-1xl bg-orange-500'>hlo</h1>
+    </>
+  )
+}
+
+export default App
